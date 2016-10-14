6bf987549739b7db876cebe8f22f67eb63feb97d
xuechao <xuechao@playcrab.com>
2016-10-14 21:22:50 +0800

    add HelloShader

--- HelloShader/HelloShader/main.cpp
+++ HelloShader/HelloShader/main.cpp
@@ -1,12 +1,13 @@
 //
 //  main.cpp
-//  HelloTriangle
+//  HelloShader
 //
-//  Created by playcrab on 16/10/11.
+//  Created by playcrab on 16/10/14.
 //  Copyright © 2016年 playcrab. All rights reserved.
 //
-#include <iostream>
 
+#include <iostream>
+#include <cmath>
 #define GLEW_STATIC
 
 #include "GL/glew.h"
@@ -58,17 +59,22 @@ int main(int argc, const char * argv[]) {
     const GLchar* verticesShaderSrc =
     "#version 330 core\n"
     "layout (location = 0) in vec3 position;\n"
+    "layout (location = 1) in vec3 color;\n"
+    "out vec3 vertexColor;\n"
+//    "uniform vec4 ourColor;\n"
     "void main()\n"
     "{\n"
     "gl_Position = vec4(position.x,position.y,position.z,1.0);\n"
+    "vertexColor = color;\n"
     "}";
     
     const GLchar* fragmentShaderSrc =
     "#version 330 core\n"
-    "out vec4 color;"
+    "out vec4 color;\n"
+    "in vec3 vertexColor;\n"
     "void main()\n"
     "{\n"
-    "color = vec4(1.0,0.5,0.2,1.0);\n"
+    "color = vec4(vertexColor,1.0f);\n"
     "}\n";
     
     
@@ -112,7 +118,7 @@ int main(int argc, const char * argv[]) {
     glAttachShader(shaderProgram,verticesShader);
     glAttachShader(shaderProgram,fragmentShader);
     
-//    glUseProgram(shaderProgram);
+    //    glUseProgram(shaderProgram);
     glLinkProgram(shaderProgram);
     
     glGetProgramiv(shaderProgram,GL_LINK_STATUS,&success);
@@ -132,20 +138,28 @@ int main(int argc, const char * argv[]) {
     
     //================== 准备数据 ============================
     
-    GLfloat vertices[]={
-//        -0.5f,-0.5f,0.0f,
+//    GLfloat vertices[]={
+//        //        -0.5f,-0.5f,0.0f,
+//        //        0.5f,-0.5f,0.0f,
+//        //        0.0f,0.5f,0.0f,
+//        0.5f,0.5f,0.0f,
 //        0.5f,-0.5f,0.0f,
-//        0.0f,0.5f,0.0f,
-        0.5f,0.5f,0.0f,
-        0.5f,-0.5f,0.0f,
-        -0.5f,-0.5f,0.0f,
-        -0.5f,0.5f,0.0f,
+//        -0.5f,-0.5f,0.0f,
+//        -0.5f,0.5f,0.0f,
+//    };
+    
+    GLfloat vertices[]={
+        //位置          //颜色
+        0.5f,-0.5f,0.0f, 1.0f,0.0f,0.0f,
+        -0.5f,-0.5f,0.0f, 0.0f,1.0f,0.0f,
+        0.0f, 0.5f,0.0f,  0.0f,0.0f,1.0f,
     };
     
-    GLuint indices[]={
-        0,1,3,
-        1,2,3
-    };
+    
+//    GLuint indices[]={
+//        0,1,3,
+//        1,2,3
+//    };
     
     GLuint vao;
     glGenVertexArrays(1,&vao);
@@ -156,16 +170,19 @@ int main(int argc, const char * argv[]) {
     glBindBuffer(GL_ARRAY_BUFFER,vbo);
     glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
     
-    glVertexAttribPointer(0,3,GL_FLOAT,GL_TRUE,3 * sizeof(GLfloat),(GLvoid*)0);
+    //
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6 * sizeof(GLfloat),(GLvoid*)0);
+    glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6 * sizeof(GLfloat),(GLvoid*)(3*sizeof(GLfloat)));
     
-    GLuint ebo;
-    glGenBuffers(1,&ebo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
-
+//    GLuint ebo;
+//    glGenBuffers(1,&ebo);
+//    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ebo);
+//    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
+    
     
     
     glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
     glBindVertexArray(0);
     
     
@@ -176,12 +193,19 @@ int main(int argc, const char * argv[]) {
         glClearColor(0.2f,0.3f,0.3f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
+//        GLfloat time =  glfwGetTime();
+//        GLfloat greenValue = sin(time) / 2 + 0.5;
+//        
+//        GLint vertexColorLocation = glGetUniformLocation(shaderProgram,"ourColor");
+        //必须先UseProgram 才能 glUniform4f;
         glUseProgram(shaderProgram);
+        
+//        glUniform4f(vertexColorLocation,0.0f,greenValue,0.0f,1.0f);
         
         glBindVertexArray(vao);
         
-//        glDrawArrays(GL_TRIANGLES,1,3);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,0);
+        glDrawArrays(GL_TRIANGLES,0,3);
+//        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT,0);
         
         glBindVertexArray(0);
         
@@ -203,3 +227,4 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action , int mo
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
+
